0ff699e7352272d17c1f7919db028382bab72b31
Evgenii Dolgov <navarus.n@gmail.com>
2024-09-11 09:35:30 +0300

    initial commit

---
+++ src/main/java/apps/amaralus/orchdemo/OrchDemoApplication.java
@@ -0,0 +1,13 @@
+package apps.amaralus.orchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrchDemoApplication.class, args);
+    }
+
+}
