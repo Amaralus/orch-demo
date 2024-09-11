43fe65192fbcd01933e719a29830d23a96b27d0f
Evgenii Dolgov <navarus.n@gmail.com>
2024-09-11 16:13:03 +0300

    added seda with dynamic routing

--- core/src/main/java/orchdemo/TestingRoute.java
+++ core/src/main/java/orchdemo/TestingRoute.java
@@ -1,5 +1,6 @@
 package orchdemo;
 
+import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +10,25 @@ public class TestingRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         rest("core")
-                .get("test").to("direct:test");
+                .get("test/{id}").to("seda:test");
 
-        from("direct:test")
-                .transform().constant("hello");
+        from("seda:test")
+                .routingSlip().method(TestingRoute.DynamicRouter.class, "route");
+
+        from("seda:bar")
+                .transform().simple("bar ${header.id}");
+
+        from("seda:baz")
+                .transform().simple("baz ${header.id}");
+    }
+
+    @Component
+    public static class DynamicRouter {
+
+        public String route(Message message) {
+            return "123".equals(message.getHeader("id"))
+                    ? "seda:bar"
+                    : "seda:baz";
+        }
     }
 }
